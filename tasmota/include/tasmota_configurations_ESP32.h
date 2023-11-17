27963a7254b90e278250f4b0aa6a222a217ce781
Zhuohui Yu <agassi21@gmail.com>
2023-11-17 15:01:52 +0800

    tasmota32 add sr04

--- tasmota/include/tasmota_configurations_ESP32.h
+++ tasmota/include/tasmota_configurations_ESP32.h
@@ -307,6 +307,7 @@
   #define USE_BLE_ESP32                          // Enable full BLE driver
   #define USE_EQ3_ESP32
   #define USE_MI_ESP32                           // (ESP32 only) Add support for ESP32 as a BLE-bridge (+9k2 mem, +292k flash)
+  #define USE_SR04
 #endif // enable USE_MI_HOMEKIT
 
 #endif  // FIRMWARE_BLUETOOTH
--- tasmota/user_config_override_sample.h
+++ tasmota/user_config_override_sample.h
@@ -91,45 +91,5 @@ Examples :
 #define USE_SR04                                 // Add support for HC-SR04 ultrasonic devices (+1k code)
  #define SR04_MAX_SENSOR_DISTANCE  500          // Set sensor max detection distance
 
-#ifdef USE_IR_REMOTE
-#undef USE_IR_REMOTE
-#endif
-
-#ifdef USE_IR_SEND_NEC
-#undef USE_IR_SEND_NEC
-#endif
-
-#ifdef USE_IR_SEND_RC5
-#undef USE_IR_SEND_RC5
-#endif
-
-
-#ifdef USE_IR_SEND_RC6
-#undef USE_IR_SEND_RC6
-#endif
-
-
-#ifdef USE_IR_RECEIVE
-#undef USE_IR_RECEIVE
-#endif
-
-
-#ifdef USE_IR_SEND_NEC
-#undef USE_IR_SEND_NEC
-#endif
-
-#ifdef USE_IR_SEND_NEC
-#undef USE_IR_SEND_NEC
-#endif
-
-#ifdef USE_IR_SEND_NEC
-#undef USE_IR_SEND_NEC
-#endif
-
-#ifdef USE_IR_SEND_NEC
-#undef USE_IR_SEND_NEC
-#endif
-
-
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
