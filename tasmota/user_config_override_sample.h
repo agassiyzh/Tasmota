b1f0571592f6de25252d66a4c806f739cd2b6991
Zhuohui Yu <agassi21@gmail.com>
2024-07-18 13:06:28 +0800

    c3

--- tasmota/user_config_override_sample.h
+++ tasmota/user_config_override_sample.h
@@ -87,8 +87,40 @@ Examples :
 // !!! Remember that your changes GOES AT THE BOTTOM OF THIS FILE right before the last #endif !!!
 */
 
+#define USE_SR04                                 // Add support for HC-SR04 ultrasonic devices (+1k code)
+ #define SR04_MAX_SENSOR_DISTANCE  500          // Set sensor max detection distance
+ #define USE_INA3221
+
+#undef USE_HLW8012                              // Add support for HLW8012, BL0937 or HJL-01 Energy Monitor for Sonoff Pow and WolfBlitz
+#undef USE_CSE7766                              // Add support for CSE7766 Energy Monitor for Sonoff S31 and Pow R2
+#undef USE_PZEM004T                             // Add support for PZEM004T Energy monitor (+2k code)
+#undef USE_PZEM_AC                              // Add support for PZEM014,016 Energy monitor (+1k1 code)
+#undef USE_PZEM_DC                              // Add support for PZEM003,017 Energy monitor (+1k1 code)
+#undef USE_MCP39F501                            // Add support for MCP39F501 Energy monitor as used in Shelly 2 (+3k1 code)
+#undef USE_BL09XX                               // Add support for various BL09XX Energy monitor as used in Blitzwolf SHP-10 or Sonoff Dual R3 v2 (+1k6 code)
+
+
+#undef USE_TUYA_MCU
+  #undef TUYA_DIMMER_ID
+  #undef USE_TUYA_TIME
 
 
 
+// -- Optional light modules ----------------------
+#undef USE_LIGHT                                // Add support for light control
+#undef USE_WS2812                               // WS2812 Led string using library NeoPixelBus (+5k code, +1k mem, 232 iram) - Disable by //
+  #undef USE_WS2812_RMT                        // ESP32 only, hardware RMT support (default). Specify the RMT channel 0..7. This should be preferred to software bit bang.
+  #undef USE_WS2812_CTYPE                // Color type (NEO_RGB, NEO_GRB, NEO_BRG, NEO_RBG, NEO_RGBW, NEO_GRBW)
+#undef USE_MY92X1                               // Add support for MY92X1 RGBCW led controller as used in Sonoff B1, Ailight and Lohas
+#undef USE_SM16716                              // Add support for SM16716 RGB LED controller (+0k7 code)
+#undef USE_SM2135                               // Add support for SM2135 RGBCW led control as used in Action LSC (+0k6 code)
+#undef USE_SM2335                               // Add support for SM2335 RGBCW led control as used in SwitchBot Color Bulb (+0k7 code)
+#undef USE_BP1658CJ                             // Add support for BP1658CJ RGBCW led control as used in Orein OS0100411267 Bulb
+#undef USE_BP5758D                              // Add support for BP5758D RGBCW led control as used in some Tuya lightbulbs (+0k8 code)
+#undef USE_SONOFF_L1                            // Add support for Sonoff L1 led control
+#undef USE_ELECTRIQ_MOODL                       // Add support for ElectriQ iQ-wifiMOODL RGBW LED controller (+0k3 code)
+#undef USE_LIGHT_PALETTE                        // Add support for color palette (+0k7 code)
+#undef USE_LIGHT_VIRTUAL_CT                     // Add support for Virtual White Color Temperature (+1.1k code)
+#undef USE_DGR_LIGHT_SEQUENCE                   // Add support for device group light sequencing (requires USE_DEVICE_GROUPS) (+0k2 code)
 
 #endif  // _USER_CONFIG_OVERRIDE_H_
